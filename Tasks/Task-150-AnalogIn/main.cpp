c045c1ad5fcdda84297b39f7636aed8d576be43c
GriffinSam <samuel.griffin@students.plymouth.ac.uk>
2021-11-09 12:31:16 +0000

    150 done

--- Tasks/Task-150-AnalogIn/main.cpp
+++ Tasks/Task-150-AnalogIn/main.cpp
@@ -3,6 +3,10 @@
 #include <cstdint>
 using namespace uop_msb;
 using namespace chrono;
+//BusOut lights(PC_2, PC_3, PC_6);
+DigitalOut greenLED(PC_6);
+DigitalOut yellowLED(PC_3);
+DigitalOut redLED(PC_2);
 
 //Fun output stuff
 LCD_16X2_DISPLAY disp;
@@ -13,6 +17,32 @@ LatchedLED ledDisp(LatchedLED::STRIP);
 AnalogIn pot(AN_POT_PIN);
 AnalogIn ldr(AN_LDR_PIN);
 AnalogIn mic(MIC_AN_PIN);
+void redbar (){
+ledDisp.setGroup(LatchedLED::LEDGROUP::RED);
+    for (unsigned int n=0; n<8; n++) {
+        ledDisp = 1 << n;
+        wait_us(250000);
+    }
+    ledDisp = 0;
+}
+void greenbar (){
+ledDisp.setGroup(LatchedLED::LEDGROUP::GREEN);
+    for (unsigned int n=0; n<8; n++) {
+        ledDisp = 1 << n;
+        wait_us(250000);
+    }
+    ledDisp = 0;
+}
+void bluebar(){
+ledDisp.setGroup(LatchedLED::LEDGROUP::BLUE);
+    for (unsigned int n=0; n<8; n++) {
+        ledDisp = 1 << n;
+        wait_us(250000);
+    }     
+    ledDisp = 0;
+}
+
+
 
 int main()
 {
@@ -20,40 +50,35 @@ int main()
     //Test LED Bar Display
     ledDisp.enable(true);
 
-    ledDisp.setGroup(LatchedLED::LEDGROUP::RED);
-    for (unsigned int n=0; n<8; n++) {
-        ledDisp = 1 << n;
-        wait_us(250000);
-    }
-    ledDisp = 0;
-
-    ledDisp.setGroup(LatchedLED::LEDGROUP::GREEN);
-    for (unsigned int n=0; n<8; n++) {
-        ledDisp = 1 << n;
-        wait_us(250000);
-    }
-    ledDisp = 0;
     
-    ledDisp.setGroup(LatchedLED::LEDGROUP::BLUE);
-    for (unsigned int n=0; n<8; n++) {
-        ledDisp = 1 << n;
-        wait_us(250000);
-    }     
-    ledDisp = 0;
+
+    
+    
+    
 
 
     while (true) {
         //Read Analog to Digital Converter values (16 bit)
+        redLED = 0;
         unsigned short potVal   = pot.read_u16();
         unsigned short lightVal = ldr.read_u16();
         unsigned short micVal   = mic.read_u16(); 
 
         //Write to terminal
         printf("--------------------------------\n");
-        printf("Potentiometer: %X\n", potVal);
+        printf("Potentiometer: %x\n", potVal);
         printf("Light Dependant Resistor: %X\n", lightVal);
         printf("Microphone: %X\n", micVal);   
+        if (lightVal < 0x2000) {
+            greenbar();}
 
+        if (potVal > 0x8000){
+           redbar();
+        }
+        if (micVal > 0x9200){
+            bluebar();
+
+        }
         //Wait 0.25 seconds
         wait_us(500000);
 
